c0e64b845742751d749f22597a656dad1a40312e
Mazhar <Mazkataz@hotmail.com>
2023-08-19 16:26:39 -0400

    check to see how far the tests go and change screenshot artifact dir

--- tests/another-test.py
+++ tests/another-test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 
-def test_nyt_homepage_loads():
+def test_msnbc_homepage_loads():
     driver = webdriver.Chrome()
     driver.get("https://www.msnbc.com/")
+    print(driver.title())
     driver.get_screenshot_as_file('artifacts/screenshots/basic-screenshot-artifact-msnbc.png')
     driver.quit()
--- tests/basic-test.py
+++ tests/basic-test.py
@@ -3,5 +3,6 @@ from selenium import webdriver
 def test_nyt_homepage_loads():
     driver = webdriver.Chrome()
     driver.get("https://www.nytimes.com/")
+    print(driver.title())
     driver.get_screenshot_as_file('artifacts/screenshots/basic-screenshot-artifact-nyt.png')
     driver.quit()
